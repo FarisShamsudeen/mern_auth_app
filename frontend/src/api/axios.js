25ace94deb9cdbc0838b83cba2282cf0d7cd61dc
Faris Shamsudeen <faris52342345@gmail.com>
2025-12-18 11:23:28 +0530

    Deleted Axios commands

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,19 +1,4 @@
 import axios from "axios";
-// import { store } from "../redux/store";
-
-// const instance = axios.create({
-//   baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
-// });
-//
-// instance.interceptors.request.use((config) => {
-//   const token = store.getState().auth.accessToken;
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-//
-// export default instance;
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
